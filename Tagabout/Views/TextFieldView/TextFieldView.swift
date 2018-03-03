7cd3064326cd8ba395d62e5f7a4e1d91295fbe1d
Karun Pant <pantkarun75@gmail.com>
2018-03-04 00:33:45 +0530

    Changes made for IBDesginable.

--- Tagabout/Views/TextFieldView/TextFieldView.swift
+++ Tagabout/Views/TextFieldView/TextFieldView.swift
@@ -9,46 +9,40 @@
 import UIKit
 
 
-class TextFieldView: UIView {
-
-    private let nibName = "TextFieldView"
-    private var contentView: UIView?
+class TextFieldView: DesignableView {
     
     @IBOutlet private weak var label: UILabel!
     @IBOutlet private weak var textField: UITextField!
     
     private var key = ""
-    var secureEntry: Bool = false
+    public var secureEntry: Bool = false
     
-    func setHeader(_ header: String, withPlaceholder placeholder: String, forKey key: String) {
+    public func setHeader(_ header: String, withPlaceholder placeholder: String, forKey key: String) {
         label.text = header
         textField.placeholder = placeholder
         self.key = key
     }
     
-    func value() -> [String: String] {
-        return [key: textField.text ?? ""]
+    public var value: [String: String] {
+        get{
+            return [key: textField.text ?? ""]
+        }
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        viewSetup()
+    }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
-        guard let view = loadViewFromNib() else { return }
-        view.frame = self.bounds
-        self.addSubview(view)
-        
+        viewSetup()
+    }
+    
+    fileprivate func viewSetup(){
         textField.layer.cornerRadius = 8.0
         textField.layer.borderWidth = 1.0
         textField.layer.borderColor = UIColor(white: 0.7, alpha: 1.0).cgColor
         textField.layer.masksToBounds = true
-        
-        contentView = view
-    }
-    
-    func loadViewFromNib() -> UIView? {
-        let bundle = Bundle(for: type(of: self))
-        let nib = UINib(nibName: nibName, bundle: bundle)
-        return nib.instantiate(withOwner: self, options: nil).first as? UIView
     }
 
 }
