3b1e237a873a711b8e427ea059f2eb290854b123
Madanlal Arora <madanlal.arora1991@gmail.com>
2018-03-11 22:24:40 +0530

    Added route to suggestion list, api call

--- Tagabout/API Handlers/APIManager.swift
+++ Tagabout/API Handlers/APIManager.swift
@@ -27,8 +27,23 @@ struct APIManager {
         }
         return task
     }
-    static func doGet(){
+    static func doGet(request: URLRequest, completion:(([String : Any]?)->())?, onError: ((Error)->())?) -> URLSessionTask? {
         //request.setValue("application/json; charset=UTF-8" , forHTTPHeaderField: "Content-Type")
-        
+        var request = request
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let task = APIGateway.shared.doDataCall(request: request, completion: { (data) in
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let json = json as? [String : Any], let completion = completion { completion(json) }
+            } catch {
+                if let completion = completion { completion(nil) }
+                print("Unexpected error: \(error).")
+                
+            }
+        }) { (error) in
+            if let onError = onError{ onError(error) }
+        }
+        return task
     }
 }
